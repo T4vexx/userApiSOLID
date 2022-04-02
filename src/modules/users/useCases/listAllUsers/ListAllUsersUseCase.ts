a90704a8fe39a1c83a9efd4688397a62cdc2d4f0
Otávio Augusto <tavinteixeira@hotmail.com>
2022-04-02 14:50:41 -0300

    Projeto Final

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,30 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const user = this.usersRepository.findById(user_id);
+
+        if (!user) {
+            throw new Error(
+                "Requisições de usuários só podem ser feitas por usuários validos"
+            );
+        }
+        if (user.admin === false) {
+            throw new Error(
+                "Somente usuários admins podem requisitar a lista de todos os usuários"
+            );
+        }
+
+        const listAllUsers = this.usersRepository.list();
+
+        return listAllUsers;
+    }
 }
 
 export { ListAllUsersUseCase };
